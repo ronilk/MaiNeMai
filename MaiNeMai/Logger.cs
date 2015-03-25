611705db343c2829ffcb36ef3431fe43337b7f22
ronilk <ronil_k@yahoo.com>
2015-03-25 22:19:03 +0530

    commiting logger

---
+++ MaiNeMai/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace MaiNeMai
+{
+    public class Logger
+    {
+        public static void LogMessage(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(@"F:\WinAppProjects\MaiNeMai\maiLog.txt", true))
+            {
+                sw.WriteLine(message + " " + DateTime.Now.ToString());
+            }
+        }
+    }
+}
